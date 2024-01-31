45ac337a2570557cd0c9737dc34b2d934fbe9ab3
Felihum <121069824+Felihum@users.noreply.github.com>
2024-01-31 14:13:26 -0300

    ProjectDescobrir funcionando

--- src/components/AddFile/AddFile.jsx
+++ src/components/AddFile/AddFile.jsx
@@ -1,17 +1,57 @@
+import { useState } from "react";
 import CollectionIcon from "../CollectionIcon/CollectionIcon";
 import "./AddFile.css"
+import { useDropzone } from "react-dropzone";
+import { useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
-function AddFile() {
-    return (
-        <div className="add-file-container">
-            <CollectionIcon />
-            <div className="text-container">
-                <div className="text-compartilhe-container">
-                    <p className="text-compartilhe">Compartilhe seu talento com milhares de pessoas</p>
-                </div>
+function AddFile(props) {
+
+    const [files, setFiles] = useState([]);
+
+    const { getRootProps, getInputProps} = useDropzone({
+        accept: {
+            'image/*': []
+        },
+        onDrop: acceptedFiles => {
+            setFiles(acceptedFiles.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            })));
+        }
+    });
+
+    const thumbs = files.map(file => (
+        <div className="thumb" key={file.name}>
+            <div className="thumbInner">
+                <img
+                src={file.preview}
+                className="img-thumb"
+                // Revoke data uri after image is loaded
+                onLoad={() => { URL.revokeObjectURL(file.preview) }}
+                />
             </div>
         </div>
+    ));
+
+    useEffect(() => {
+        return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+    }, []);
+
+    return (
+        <section>
+            <div {...getRootProps({className: "add-file-container"})}>
+                <input {...getInputProps()} />
+                <CollectionIcon />
+                <div className="thumbsContainer">
+                    {thumbs}
+                </div>
+                <div className="text-container">
+                    <div className="text-compartilhe-container">
+                        <p className="text-compartilhe">Compartilhe seu talento com milhares de pessoas</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
