a04b56e5a0e7a06ae9c27a53f2058fe60848c1e0
Felihum <121069824+Felihum@users.noreply.github.com>
2024-02-05 22:49:17 -0300

    Url base

--- src/scripts/Api/api.js
+++ src/scripts/Api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:8080"
+const URL = "https://cc90-2804-868-d057-760b-2ded-1f6c-5efe-73c0.ngrok-free.app"
 
 const api = axios.create({
     baseURL: URL,
